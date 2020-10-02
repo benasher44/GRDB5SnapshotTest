de82fddf77b398dc4a64dd5a2f41bcdebdb3bb4d
Ben Asher <ben.asher@autodesk.com>
2020-10-02 09:12:33 -0700

    Initial Commit

---
+++ GRDB5SnapshotTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GRDB5SnapshotTest
+//
+//  Created by Ben Asher on 10/2/20.
+//  Copyright © 2020 Ben Asher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
